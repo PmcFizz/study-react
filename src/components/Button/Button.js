813fdf70b5609bd8138870c94f13e809958a5353
pmc <pangmengchen@gmail.com>
2020-09-30 14:10:00 +0800

    feat: 测试文件
    Signed-off-by: pmc <pangmengchen@gmail.com>

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React ,{Component} from 'react'
+import './Button.css'
+
+class Button extends Component {
+  render() {
+    return <div className="Button">按钮组件</div>
+  }
+}
+
+export default Button
